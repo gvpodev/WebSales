b6b4536bf9be534667c8b81532df64b0c14037d8
Gabriel Vasconcellos <gavapim@gmail.com>
2020-03-24 17:15:27 -0300

    Adicao das outras entidades e Migration

--- WebSalesMVC/Data/WebSalesMVCContext.cs
+++ WebSalesMVC/Data/WebSalesMVCContext.cs
@@ -13,6 +13,8 @@ namespace WebSalesMVC.Models
         {
         }
 
-        public DbSet<WebSalesMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ WebSalesMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebSalesMVC.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Penging = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
