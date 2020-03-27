2dfd9904116c4d4d3af709b153d8b8b85435600d
Gabriel Vasconcellos <gavapim@gmail.com>
2020-03-26 21:05:29 -0300

    Deletar Seller

--- WebSalesMVC/Controllers/SellersController.cs
+++ WebSalesMVC/Controllers/SellersController.cs
@@ -35,11 +35,36 @@ namespace WebSalesMVC.Controllers
             return View(viewModels);
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
